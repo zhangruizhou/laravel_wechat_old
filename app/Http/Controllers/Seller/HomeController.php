d80a28a4ba994e7c0a922fe8023e889a7f206cd3
zhangruizhou <zrzqq@sina.cn>
2016-10-08 19:00:32 +0800

    commit

---
+++ app/Http/Controllers/Seller/HomeController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\Seller;
+
+class HomeController extends BaseController
+{
+
+    public function __construct()
+    {
+    }
+
+    public function index(){
+        return 'index';
+    }
+}
