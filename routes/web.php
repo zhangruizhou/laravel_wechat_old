04ea85f1a6566bc69e12435dc3364b5d2ea62768
zhangruizhou <zrzqq@sina.cn>
2016-10-09 16:10:03 +0800

    commit

--- routes/web.php
+++ routes/web.php
@@ -27,10 +27,7 @@ if (strtolower($prefix) == 'wechat') {
 } else { //解决composer时自动查找localhost的问题
     $namespace = 'Front';
 }
-
-Route::get('/', function (){
-    return 'Hello Word';
-});
+echo $host;die;
 //域名分组
 Route::group(array('domain' => $host, 'namespace'=>$namespace), function() use($namespace){
     return  __DIR__.'/Group/' . $namespace .'.php';
