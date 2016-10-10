e008b6a5421fa2aefd995de1d7a453b5962ba1e0
zhangruizhou <zrzqq@sina.cn>
2016-10-10 11:45:46 +0800

    commit

---
+++ app/Http/Middleware/SellerMiddleware.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Support\Facades\Session;
+
+class SellerMiddleware{
+
+    /**
+     * Handle an incoming request.
+     *
+     * @param \Illuminate\Http\Request  $request
+     * @param \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        $user = Session::get('user.passport');
+
+        if (($user && $user['role'] != 0) or $request->is('passport/login') or $request->is('passport/logout')) {
+            return $next($request);
+        }
+        return redirect('passport/login');
+    }
+}
